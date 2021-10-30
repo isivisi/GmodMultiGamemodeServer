f47bfdeb77d660773ac989a9431ab1d19119bb54
John Iannandrea <jiannandrea@gmail.com>
2021-10-29 21:40:34 -0400

    Update SetGameMode.lua

--- SetGameMode.lua
+++ SetGameMode.lua
@@ -14,12 +14,14 @@ mapGameModes['murder_'] ="murder"
 mapGameModes['md_'] ="murder"
 mapGameModes['slender_'] = "stopitslender"
 
+hostName = "Variety Gmod Server"
+
 map = game.GetMap()
 gamemode = gmod.GetGamemode().FolderName
 
 for pre, mode in pairs(mapGameModes) do
 	if (string.match(map, pre)) then
-		RunConsoleCommand("hostname", "["..gmod.GetGamemode().Name.."]"..hostName)
+		RunConsoleCommand("hostname", "["..gmod.GetGamemode().Name.."] "..hostName)
 		if (gamemode != mode) then
 			print("changing mode")
 			print(mode)
